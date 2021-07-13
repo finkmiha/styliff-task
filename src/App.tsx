90f02851a08bafc42097e8ba252afdbbf7e9803c
Miha <mihafink333@gmail.com>
2021-07-13 22:36:45 +0200

    Main app file update.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import RepoTable from './tables/repo-table';
+import styliffLogo from './assets/images/styliff-logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <img
+          className="header-image"
+          src={styliffLogo}
+          alt="header logo"
+        />
+        <p className="header-title">
+          GitHub repository search
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <RepoTable />
     </div>
   );
 }
